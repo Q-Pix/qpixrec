efba2b575a43352d5536a1da1d11fd0d33c2b955
Dave Elofson <dave.elofson@tamu.edu>
2023-08-03 14:13:34 -0500

    gutting the repository to start over, and correctly

--- exceptions.py
+++
@@ -1,9 +0,0 @@
-class Error(Exception):
-    """Base class for other exceptions"""
-    pass
-
-class DataNotPresentError(Error):
-    """No reset data is present in the given directory"""
-    def __init__(self, message="No reset data file (resets_output.txt) is present in the given directory"):
-        self.message = message
-        super().__init__(self.message)
