328da9faf3a9076e896f287ffa87e03c2b2c063f
Carter Eikenbary <eikenbcr@tamu.edu>
2023-08-04 14:15:31 -0500

    Update t0_hitmaker.py

--- lightless_reconstruction/t0_hitmaker.py
+++ lightless_reconstruction/t0_hitmaker.py
@@ -276,8 +276,9 @@ for n in range(total_events):
     legend = plt.legend()
 
     # Set the marker size for the second legend entry (index 1, since it is zero-based)
-    legend.legendHandles[1]._sizes = [10]
+    legend.legendHandles[1]._sizes = [10
+                                     
     if len(sys.argv) > 6:
-        if sys.argv[6] == "-v":
+        if sys.argv[6] == "-v"  or sys.argv[6] == "-verbose":
             plt.savefig(t0_hitmaker_dir + '/yz_reconstruction_Event' + str(n) + '.png')
     plt.close()
